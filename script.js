f6d9e94cb19d06f69689881cec72bef36da992ac
Inaki21v <125582696+Inaki21v@users.noreply.github.com>
2023-02-16 16:21:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+console.log("from script file");
+// Obtener los botones por su ID
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var btn3 = document.getElementById("btn3");
+var btn4 = document.getElementById("btn4");
+var btn5 = document.getElementById("btn5");
+var btn6 = document.getElementById("btn6");
+var btn7 = document.getElementById("btn7");
+var btn8 = document.getElementById("btn8");
+var btn9 = document.getElementById("btn9");
+
+// Asignar funciones a los botones
+btn1.addEventListener("click", function() {
+  window.location.href = "https://schoolcheats.net/";
+});
+
+btn2.addEventListener("click", function() {
+  window.location.href = "https://www.programiz.com/python-programming/online-compiler/";
+});
+
+btn3.addEventListener("click", function() {
+  window.location.href = "https://www.facebook.com/";
+});
+
+btn4.addEventListener("click", function() {
+  window.location.href = "https://www.twitter.com/";
+});
+
+btn5.addEventListener("click", function() {
+  window.location.href = "https://www.instagram.com/";
+});
+
+btn6.addEventListener("click", function() {
+  window.location.href = "https://www.linkedin.com/";
+});
+
+btn7.addEventListener("click", function() {
+  window.location.href = "https://www.github.com/";
+});
+
+btn8.addEventListener("click", function() {
+  window.location.href = "https://www.wikipedia.org/";
+});
+
+btn9.addEventListener("click", function() {
+  window.location.href = "https://www.amazon.com/";
+});
